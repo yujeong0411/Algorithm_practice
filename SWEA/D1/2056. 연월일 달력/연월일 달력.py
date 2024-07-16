7fdabced706ad6af242285d8aaa1051735521ea0
yujeong0411 <choiyujeong0411@gmail.com>
2024-07-16 21:21:47 +0900

    [D1] Title: 연월일 달력, Time: 103 ms, Memory: 43,776 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.py
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.py
@@ -6,10 +6,10 @@ for test_case in range(1, T + 1):
     day = num[6:]
     end_day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 월의 마지막일 
 
-   
-    if 0 < int(month) < 13 and 0 < int(day) <= end_day[int(month)]:   # str을 int로 변환하여 1~12월 제외 가리기
-        print(f'#{test_case} {year}/{month}/{day}')
-    else:
-        print(f'#{test_case} -1')
+    result = year+"/"+month+"/"+day
+    if int(month) <= 0 or int(month) > 12:   # str을 int로 변환하여 1~12월 제외 가리기
+        result = '-1'
+    elif int(day) <= 0 or int(day) > end_day[int(month)]:   # 마지막일의 범위를 벗어난 것 가리기
+        result = '-1'
 
-
+    print(f'#{test_case} {result}')
