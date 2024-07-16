d2f8f3dabed1800f83f7786cb67134a72c0da67b
yujeong0411 <choiyujeong0411@gmail.com>
2024-07-16 21:31:30 +0900

    Update 연월일 달력.py

--- SWEA/D1/2056. 연월일 달력/연월일 달력.py
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.py
@@ -13,3 +13,19 @@ for test_case in range(1, T + 1):
         result = '-1'
 
     print(f'#{test_case} {result}')
+
+#####################################################################################
+# Solution 2 (방식만 다르게)
+T = int(input())
+for test_case in range(1, T + 1):
+    num = str(input())                 # str으로 날짜 받기
+    year = num[0:4]                    # 0000/00/00 날짜 단위로 인덱스 끊기
+    month = num[4:6]
+    day = num[6:]
+    end_day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 월의 마지막일 
+
+   
+    if 0 < int(month) < 13 and 0 < int(day) <= end_day[int(month)]:  # 제외가 아닌 해당사항 맞추기
+        print(f'#{test_case} {year}/{month}/{day}')
+    else:
+        print(f'#{test_case} -1')
