ab4a7475151bf0053ba27e895848733d14eb914b
yujeong0411 <choiyujeong0411@gmail.com>
2024-08-15 15:25:52 +0900

    [D2] Title: 달팽이 숫자, Time: 102 ms, Memory: 42,984 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,30 +1,25 @@
 T = int(input())
-for test_case in range(1, T + 1):
-    N = int(input()) 
-    
-    # 0으로 된 N*N 배열 생성
-    arr = [[0 for _ in range(N)] for _ in range(N)]
+for test_case in range(1, T+1):
+    N = int(input())
+    arr = [[0]*N for _ in range(N)]
+    di = [0, 1, 0, -1]  # 우, 하, 좌, 상
+    dj = [1,0, -1, 0]
+    d_idx = 0   # 델타 인덱스
 
-    # x축, y축 방향 전환
-    di = [0, 1 ,0, -1]
-    dj = [1, 0 ,-1, 0]
+    arr[0][0] = 1  # 첫번째 1로 설정
+    i = j = 0
+    num = 2
+    while num <= N*N:   # 배열 안 숫자가 N*N보다 작을때 까지
+        ni = i + di[d_idx]
+        nj = j + dj[d_idx]
+        # 배열을 벗어나지 않게 조건 설정
+        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 0:
+            arr[ni][nj] = num
+            i, j = ni, nj   # i, j 업데이트
+            num += 1
+        else:
+            d_idx = (1+d_idx) % 4
 
-    num = 1
-    i, j = 0, 0
-    d_idx = 0  # di, dj의 인덱스
-    arr[i][j]= num   # 먼저 (0,0) 값 지정
-    num += 1
-    while num <= N*N:   # num는 배열의 칸 갯수까지 가능
-        i_idx = i + di[d_idx]  # 다음 순서 인덱스
-        j_idx = j + dj[d_idx]
-        if 0 <= i_idx < N and 0 <= j_idx < N and arr[i_idx][j_idx] == 0:
-                i, j = i_idx, j_idx
-                arr[i][j]= num  
-                num += 1
-        else:       # 배열 방향 전환
-            d_idx = (d_idx + 1) % 4  # d_idx 0에서 3까지 인덱스 돌리기
-        
     print(f'#{test_case}')
-    for list in arr:
-        print(*list)
-        
+    for lst in arr:
+        print(*lst)
