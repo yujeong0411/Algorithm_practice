2bc80d93139e880c7668e55921f702994a16f63a
yujeong0411 <choiyujeong0411@gmail.com>
2024-08-18 20:00:14 +0900

    [Silver IV] Title: 색종이 - 2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,20 +1,22 @@
-def count(arr):
+def c(arr):
     cnt = 0
     for lst in arr:
         for i in range(1, len(lst)):
-            if lst[i-1]!=lst[i]:  # 현재값과 직전의 값이 다르면 경계선!
-                cnt+=1
+            if lst[i] != lst[i - 1]:
+                cnt += 1
     return cnt
 
-N = int(input())
-arr = [[0]*102 for _ in range(102)]
+N = int(input())  # 색종이 수
+arr = [[0]*101 for _ in range(101)]
 for _ in range(N):
-    # [1] 해당 영역을 1로 표시
-    sj, si = map(int, input().split())
-    for i in range(si, si+10):
-        for j in range(sj, sj+10):
-            arr[i][j]=1
+    x, y = map(int, input().split())  # 색종이 위치
+    for i in range(y, 10+y):
+        for j in range(x, 10+x):
+            arr[i][j] = 1
 
-arr_t = list(zip(*arr))  # 전치행렬: 수정필요시 list(map(list, zip(*arr)))
-ans = count(arr) + count(arr_t)
-print(ans)
+arr2 = list(map(list, zip(*arr)))
+result = c(arr) + c(arr2)
+print(result)
+
+
+
