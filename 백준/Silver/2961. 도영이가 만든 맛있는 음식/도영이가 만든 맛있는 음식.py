03cf768804502939d67eca9c4de68041d86e13fc
yujeong0411 <choiyujeong0411@gmail.com>
2024-09-18 23:36:35 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,15 +1,11 @@
-from itertools import combinations
 N = int(input())  # 재료 개수
 flavor = [list(map(int, input().split())) for _ in range(N)] # 신맛, 쓴맛
 result = 21e8  # 신맛, 쓴맛 차이
-# 재료 조합
-for i in range(1, N+1):  # 재료는 1개부터 N개까지 사용
-    for combs in combinations(flavor, i):  # 재료 조합
-        # print(combs)
-        S, B = 1, 0  # 신맛은 곱, 쓴맛은 합
-        for s, b in combs:
-            S *= s
-            B += b
-        result = min(result, abs(S-B))  # 최소값 갱신
+for comb in range(1, 1 << N):  # 재료는 1개부터 시작, 2^N-1가지 경우의 수
+    S, B = 1, 0  # 신맛은 곱, 쓴맛은 합
+    for i in range(N):
+        if comb & (1 << i) != 0:  # comb의 i번째 비트가 1이면 해당 재료 선택
+            S *= flavor[i][0]
+            B += flavor[i][1]
+    result = min(result, abs(S-B))
 print(result)
-
