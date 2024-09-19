607f502bef5e4c1eb32f172da154754f675600fb
yujeong0411 <choiyujeong0411@gmail.com>
2024-09-19 23:36:35 +0900

    [Silver I] Title: 스타트와 링크, Time: 556 ms, Memory: 110460 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,24 +1,25 @@
-def member(num, start, link):
-    global result
-    if num == N:  # N명이 모두 팀을 나누었을 때
-        if len(start) == N//2:  # 스타트팀의 멤버가 N//2명일때
-            s_stat = l_stat = 0  # 스타트, 링크 팀의 능력치 초기화
-            # 능력치 구하기
-            for i in range(N//2):
-                for j in range(N//2):
-                    s_stat += synergy[start[i]][start[j]]
-                    l_stat += synergy[link[i]][link[j]]
-            # 팀 별 능력치 차이 최소값 구하기
-            result = min(result, abs(s_stat-l_stat))
-        return
-    # 멤버 추가하기기    member(num+1, start+[num], link)
-    member(num+1, start+[num], link)
-    member(num+1, start, link+[num])
-
-
+from itertools import combinations
+# N은 항상 짝수이며, 두 팀은 각각 N//2명으로 구성
 N = int(input())
 synergy = [list(map(int, input().split())) for _ in range(N)]
-# N은 짝수, 팀은 N//2
 result = 21e8
-member(0, [], [])
+
+#팀 조합
+for start in combinations(range(N), N//2): # range(N)에서 N//2명의 멤보 조합 생성
+    # print(start) -> start 리스트를 만들지 않아도 for문을 통해 변수로 바로 전달된다.
+    # link = [i for i in range(N) if i not in start]
+    link = []
+    # 스타트 팀이 아닌 경우 링크 팀에 추가
+    for i in range(N):
+        if i not in start:
+            link.append(i)
+
+    # 능력치 구하기
+    s_stat = l_stat = 0  # 스타트, 링크 팀의 능력치 초기화
+    for i in range(N//2):
+        for j in range(N//2):
+            s_stat += synergy[start[i]][start[j]]
+            l_stat += synergy[link[i]][link[j]]
+    # 팀 별 능력치 차이 최소값 구하기
+    result = min(result, abs(s_stat-l_stat))
 print(result)
