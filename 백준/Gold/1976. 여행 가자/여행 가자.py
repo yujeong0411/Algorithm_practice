a3e23d62d02af2019454c86a530520f2a23ac90a
yujeong0411 <choiyujeong0411@gmail.com>
2024-10-03 16:31:36 +0900

    [Gold IV] Title: 여행 가자, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,39 +1,27 @@
 def find(x):
-    if parents[x] == x:  # x 자기자신이 x 를 바라본다 == 해당 집합의 대표자
-        return x
-    parents[x] = find(parents[x])
-    # print(parents, 'find')
+    if parents[x] != x:
+        parents[x] = find(parents[x])  # 경로 압축
     return parents[x]
 
 def union(x, y):
-    # x 와 y 의 대표자를 찾자.
     root_x = find(x)
     root_y = find(y)
-
-    if root_x == root_y:  # 이미 같은 집합이면 끝
-        return
-
-    # 다른 집합이라면 더 작은 루트노드에 합친다.
-    if root_x < root_y:
+    if root_x != root_y:  # 같은 집합이 아니면 합치기
         parents[root_y] = root_x
-    else:
-        parents[root_x] = root_y
-    # print(parents, 'union')
 
-N = int(input())
-M = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-parents = [i for i in range(N)]
+N = int(input())  # 도시 수
+M = int(input())  # 여행계획에 포함된 도시 수
+arr = [list(map(int, input().split())) for _ in range(N)]  # 도시간 연결 정보
+parents = list(range(N))  # 부모 테이블 초기화
+
+# 연결된 도시들을 같은 집합으로 묶기
 for i in range(N):
-    for j in range(N):
-        # 도시가 연결되어 있다면 
+    for j in range(i + 1, N):  # 대칭 행렬이므로 절반만 확인
         if arr[i][j] == 1:
             union(i, j)
 
-plan = list(map(int, input().split()))
-result = 'YES'
-for i in range(1, M):
-    if parents[plan[i]-1] != parents[plan[0]-1]:
-        result = 'NO'
-        break
-print(result)
+plan = list(map(int, input().split()))  # 여행 계획
+root = find(plan[0] - 1)  # 첫 번째 도시의 루트
+result = all(find(city - 1) == root for city in plan[1:])  # 모든 도시가 같은 루트인지 확인
+
+print("YES" if result else "NO")
