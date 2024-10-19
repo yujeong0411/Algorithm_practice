5553a854dcd90f399a2a0fe9b05227ffc9649425
yujeong0411 <choiyujeong0411@gmail.com>
2024-10-20 01:19:39 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 420 ms, Memory: 188068 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -1,29 +1,23 @@
-from collections import deque
+import sys
 
-N = int(input())  # 전체 시간
-current_score = current_min = 0
-assignment = deque()  # 리스트 사용 시 시간초과 남.
+N = int(input())
+arr = []
 result = 0
-for _ in range(N):
-    info = list(map(int, input().split()))
-    # print(info, 'info')
-    if current_min == 0:
-        result += current_score
-        current_score = 0
+for i in range(N):
+    lst = list(map(int, sys.stdin.readline().split()))
+    
+    if lst and lst != [0]:  
+        lst[2] -= 1
+        if lst[2] == 0:
+            result += lst[1]
+        else:
+            arr.append(lst)
+    elif arr:  # arr이 비어있지 않은 경우에만 pop 시도
+        cool = arr.pop()
+        cool[2] -= 1
+        if cool[2] == 0:
+            result += cool[1]
+        else:
+            arr.append(cool)
 
-    if info[0] == 1:
-        if current_min and current_score:
-            assignment.append((current_score, current_min))
-        current_score = info[1]
-        current_min = info[2] - 1
-    elif info[0] == 0:
-        if current_score and current_min:
-            current_min -= 1
-        elif assignment:
-            current_score, current_min = assignment.pop()
-            current_min -= 1
-    # print(current_score, current_min)
-    # print(assignment, 'assignment')
-if current_min == 0 and current_score:
-    result += current_score
 print(result)
